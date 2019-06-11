ef65660c8537f3319f20aa7fed4c70b8602bf902
Lieven <lieven_vandegaer@hotmail.com>
2019-06-11 20:59:36 +0200

    Update packages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,25 +2,17 @@
   <div class="home">
     <br>
     <select v-on:change="fetchData" v-model="sensor_id" :options="sensors">
-      <option v-for="sensor in sensors" :value="sensor.value">{{ sensor.text }}</option>
+      <option v-for="sensor in sensors" :value="sensor">{{ sensor.SDS011ID }} - {{ sensor.STREET_NAME }} {{ sensor.CITY }}</option>
     </select>
     <h4 class="ui horizontal divider header">
       <i class="bar chart icon"></i>
-      Luftdaten sensor {{ sensor_id.dust_sensor_id }}
+      Luftdaten sensor {{ sensor_id.SDS011ID }}
     </h4>
     <div class="ui two statistics">       
       <pm25component v-bind:pm25="sensor_PM25"></pm25component>
       <pm10component v-bind:pm10="sensor_PM10"></pm10component>
     </div>
     <p><i class="clock icon"></i>{{ sensor_lufdaten_timestamp_minutes }} minutes ago</p>
-    <!-- <h4 class="ui horizontal divider header">
-      <i class="bar chart icon"></i>
-      Irceline Aarschot
-    </h4>
-    <div class="ui two statistics">       
-      <pm25component v-bind:pm25="sensor_irceline_PM25"></pm25component>
-      <pm10component v-bind:pm10="sensor_irceline_PM10"></pm10component>
-    </div> -->
     <h4 class="ui horizontal divider header">
       <i class="bar chart icon"></i>
       Irceline Leuven (Interpolated)
@@ -34,7 +26,7 @@
     <div class="ui statistics">
       <h4 class="ui horizontal divider header">
         <i class="bar chart icon"></i>
-        Temperature (Luftdaten sensor {{ this.sensor_id.temp_sensor_id }})
+        Temperature (Luftdaten sensor {{ this.sensor_id.DHTID }})
       </h4>
       <div class="grey statistic">
         <div class="value">
@@ -53,13 +45,62 @@
         </div>
       </div>
     </div>
+
+    <div class="ui statistics">
+      <h4 class="ui horizontal divider header">
+        <i class="bar chart icon"></i>
+        Comparison with other Leuvenair sensors
+      </h4>
+      <div class="grey statistic">
+        <div class="value">
+          {{ sensor_comparison }}
+        </div>
+        <div class="label">
+          Based on last 5 minutes; PM2.5; 1 = cleanest sensor
+        </div>
+      </div>
+    </div>
+
+
+    <div class="ui statistics">
+      <h4 class="ui horizontal divider header">
+        <i class="bar chart icon"></i>
+        Comparison with other Leuvenair sensors
+      </h4>
+      <div class="grey statistic">
+        <div class="value">
+          {{ lowest_value }}
+        </div>
+        <div class="label">
+          Lowest value
+        </div>
+      </div>
+      <div class="grey statistic">
+        <div class="value">
+          {{ current_value }}
+        </div>
+        <div class="label">
+          Sensor value
+        </div>
+      </div>
+      <div class="grey statistic">
+        <div class="value">
+          {{ highest_value }}
+        </div>
+        <div class="label">
+          Highest value
+        </div>
+      </div>
+    </div>
+    
+
     <h4 class="ui horizontal divider header">
         <i class="bar chart icon"></i>
         History
       </h4>
-    <iframe v-bind:src="'https://maps.luftdaten.info/grafana/d-solo/000000004/single-sensor-view?orgId=1&panelId=2&var-node=' +      sensor_id.dust_sensor_id + '&from=\'' + graph_startDate + '\'&to=\'' + graph_endDate + '\''" width="100%" height="auto" frameborder="0"></iframe>
+    <iframe v-bind:src="'https://maps.luftdaten.info/grafana/d-solo/000000004/single-sensor-view?orgId=1&panelId=2&var-node=' +      sensor_id.SDS011ID + '&from=\'' + graph_startDate + '\'&to=\'' + graph_endDate + '\''" width="100%" height="auto" frameborder="0"></iframe>
     <br>
-    <iframe v-bind:src="'https://maps.luftdaten.info/grafana/d-solo/000000004/single-sensor-view?orgId=1&panelId=1&var-node=' +      sensor_id.dust_sensor_id + '&from=\'' + graph_startDate + '\'&to=\'' + graph_endDate + '\''" width="100%" height="auto" frameborder="0"></iframe>
+    <iframe v-bind:src="'https://maps.luftdaten.info/grafana/d-solo/000000004/single-sensor-view?orgId=1&panelId=1&var-node=' +      sensor_id.SDS011ID + '&from=\'' + graph_startDate + '\'&to=\'' + graph_endDate + '\''" width="100%" height="auto" frameborder="0"></iframe>
 
 
     <cookie-law theme="blood-orange--rounded"></cookie-law>
@@ -72,7 +113,7 @@
   import pm25component from '../components/pm25component.vue'
   import pm10component from '../components/pm10component.vue'
   import CookieLaw from 'vue-cookie-law'
-  import sensor_list from '../assets/sensor_list.json'
+  import sensor_list from '../assets/sensor_list_new.json'
   
   function checkTemperature(sensordatavalue) {
     return (sensordatavalue.value_type == "temperature")
@@ -92,8 +133,6 @@
 
   export default {
     name: 'home',
-    created: function () {
-    },
     data() {
       return {
         sensor_id: null,
@@ -111,7 +150,11 @@
         sensor_humidity: null,
         graph_startDate: null,
         graph_endDate: null,
-        sensors: sensor_list
+        sensors: sensor_list,
+        sensor_comparison: null,
+        lowest_value: null,
+        highest_value: null,
+        current_value: null
       }
     },
     components: { 
@@ -120,7 +163,37 @@
       CookieLaw
     },
     methods: {
+      fetchComparison: function() {
+        console.log('mapping')
+
+        var list_of_IDS = []
+        if (this.sensors != null) {
+          list_of_IDS = this.sensors.map(x => parseInt(x.SDS011ID));
+        }
+        
+        //console.log(list_of_IDS)
+
+        if (list_of_IDS.length > 0) {  
+          let my_body =  
+              {
+                "sensor": parseInt(this.sensor_id.SDS011ID),
+                "list_of_sensors": list_of_IDS
+              }
+          let my_body_in_json = JSON.stringify(my_body);
+          axios
+          .post('https://wdkops59x2.execute-api.us-east-2.amazonaws.com/default/hello-world-python', my_body_in_json )
+          .then(response => {
+            //console.log(response)
+            this.sensor_comparison = response.data.position
+            this.highest_value = response.data.highest_value
+            this.lowest_value = response.data.lowest_value
+            this.current_value = response.data.current_value
+          })
+        }
+      },
       fetchData: function() {
+        console.log('fetchData')  
+        console.log(this.sensor_id)  
 
         //calculate dates for graphs
         var nowDate = new Date()
@@ -128,19 +201,19 @@
         myDate.setHours(nowDate.getHours() - 60)
         if (this.graph_startDate == null)
           { 
-            console.log(myDate)
-            console.log(myDate.getTime()/1000|0)
+            //console.log(myDate)
+            //console.log(myDate.getTime()/1000|0)
             this.graph_startDate = myDate.getTime()/1000|0
             this.graph_endDate = nowDate.getTime()/1000|0
           }
 
-        console.log('Fetching Data')
-        //console.log('Setting the cookie');
         if (this.sensor_id !== null)
           {this.$cookies.set('sensor_id', this.sensor_id)}
-        //console.log('Fetching data...');
+
+        console.log('Fetching data...');
+
         axios
-        .get('https://api.luftdaten.info/v1/sensor/' + this.sensor_id.dust_sensor_id + '/')
+        .get('https://api.luftdaten.info/v1/sensor/' + this.sensor_id.SDS011ID + '/')
         .then(response => {
           if (response.data.length > 0) {
             //console.log(response)
@@ -167,10 +240,6 @@
         axios
         .get('https://geo.irceline.be/ows?service=WFS&version=1.3.0&request=GetFeature&typeName=rio:pm25_hmean&cql_filter=id=985&outputformat=json') //Real-time interpolated value
         .then(response => {
-          //console.log(response)
-          //console.log(response.data.features[0].properties.value)
-          //var features = response.data.features
-          //var features_with_value = features.filter(checkValueNotMinus99)
           var features_with_value_sorted = response.data.features.sort(function(a, b){return a.properties.timestamp - b.properties.timestamp})
           var feature_to_display = features_with_value_sorted[features_with_value_sorted.length - 1]
           this.sensor_irceline_PM25_interpolated = feature_to_display.properties.value
@@ -185,41 +254,16 @@
         axios
         .get('https://geo.irceline.be/ows?service=WFS&version=1.3.0&request=GetFeature&typeName=rio:pm10_hmean&cql_filter=id=985&outputformat=json') //Real-time interpolated value
         .then(response => {
-          //console.log(response)
-          //console.log(response.data.features[0].properties.value)
-          //var features = response.data.features
-          //var features_with_value = features.filter(checkValueNotMinus99)
           var features_with_value_sorted = response.data.features.sort(function(a, b){return a.properties.timestamp - b.properties.timestamp})
           var feature_to_display = features_with_value_sorted[features_with_value_sorted.length - 1]
           this.sensor_irceline_PM10_interpolated = feature_to_display.properties.value
         });
         axios
-        .get('https://geo.irceline.be/sos/api/v1/timeseries/100014') //irceline pm25 sensor
-        .then(response => {
-          if (response.data !== undefined) {
-            this.sensor_irceline_PM25 = Number(response.data.lastValue.value)
-            //this.sensor_irceline_timestamp = response.data.lastValue.timestamp
-          }
-          else {
-            this.sensor_irceline_PM25 = NaN
-            //this.sensor_irceline_timestamp = null
-          }
-        });
-        axios
-        .get('https://geo.irceline.be/sos/api/v1/timeseries/6716') //irceline pm10 sensor
-        .then(response => {
-          if (response.data !== undefined) {
-            this.sensor_irceline_PM10 = Number(response.data.lastValue.value)
-          }
-          else {
-            this.sensor_irceline_PM10 = NaN
-          }
-        });
-        axios
-        .get('https://api.luftdaten.info/v1/sensor/' + this.sensor_id.temp_sensor_id + '/')
+        .get('https://api.luftdaten.info/v1/sensor/' + this.sensor_id.DHTID + '/')
         .then(response => {
           if (response.data.length > 0) {
             this.sensor_humidity = response.data[0].sensordatavalues.find(checkHumidity).value
+            this.sensor_temp_full = response.data[0].sensordatavalues.find(checkTemperature).value
           }
           else {
             this.sensor_humidity = "..."
@@ -230,27 +274,14 @@
           console.log(error)
         }
         );
-        axios
-        .get('https://api.luftdaten.info/v1/sensor/' + this.sensor_id.temp_sensor_id + '/')
-        .then(response => {
-          if (response.data.length > 0) {
-            this.sensor_temp_full = response.data[0].sensordatavalues.find(checkTemperature).value
-          }
-          else {
-            this.sensor_temp_full = "..." 
-          }
-        })
-        .catch((error) => {
-          this.sensor_temp_full = "..."
-          console.log(error)
-        })
+        this.fetchComparison()
+
       }
     },
     updated () {
-      //this.fetchData()
+
     },
-    mounted () {
-      //reading the default sensor from a cookie
+     created () {
       console.log("reading the cookie");
       if (this.$cookies.isKey('sensor_id'))
       {
@@ -259,12 +290,27 @@
       else
       { 
         this.sensor_id = { 
-          dust_sensor_id: 12030,
-          temp_sensor_id: 12031
+          "SDS011ID": 12030,
+          "DHTID": 12031
         }
       }
-      console.log("cookie content: " + this.$cookies.get('sensor_id').dust_sensor_id);
-      this.fetchData()
+
+      console.log("reading the sensor list");
+
+      axios
+      .get('https://cors-anywhere.herokuapp.com:443/leuvenair.be/grabmeta.php?active=1')
+      .then(response => {
+        if (response.data.length > 0) {
+            this.sensors = response.data
+            console.log('sensor list updated')
+          }
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+       this.fetchData()
+     },
+    mounted () {
     }
   }
 </script>
